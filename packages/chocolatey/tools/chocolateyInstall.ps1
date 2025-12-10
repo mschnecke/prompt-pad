e0a7d5c10abddbf3ec2e59caf77cdd486cf3cff7
Michael Schnecke <michael.schnecke@cortado.com>
2025-12-10 16:15:31 +0100

    Switch Windows installer from NSIS to MSI format
    This update transitions the Windows installer from NSIS `.exe` to MSI `.msi` for improved compatibility and simplified installation. Adjustments were made to workflow files, Chocolatey scripts, and configuration to support the new format, including silent install arguments and unique product codes.

--- packages/chocolatey/tools/chocolateyInstall.ps1
+++ packages/chocolatey/tools/chocolateyInstall.ps1
@@ -5,24 +5,16 @@ $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName    = $packageName
-  fileType       = 'exe'
-  url64bit       = 'https://github.com/mschnecke/prompt-pad/releases/download/v1.0.0/PromptPad_1.0.0_x64-setup.exe'
+  fileType       = 'msi'
+  url64bit       = 'https://github.com/mschnecke/prompt-pad/releases/download/v1.0.0/PromptPad_1.0.0_x64_en-US.msi'
   softwareName   = 'PromptPad*'
   checksum64     = 'PLACEHOLDER_SHA256'
   checksumType64 = 'sha256'
-  silentArgs     = '/S'
-  validExitCodes = @(0)
+  silentArgs     = '/qn /norestart'
+  validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
-
-# Create Start Menu shortcut
-$shortcutPath = Join-Path $env:ProgramData 'Microsoft\Windows\Start Menu\Programs\PromptPad.lnk'
-$targetPath = Join-Path $env:LOCALAPPDATA 'Programs\PromptPad\PromptPad.exe'
-
-if (Test-Path $targetPath) {
-  Install-ChocolateyShortcut -ShortcutFilePath $shortcutPath -TargetPath $targetPath
-}
 
 Write-Host @"
 
--- packages/chocolatey/tools/chocolateyUninstall.ps1
+++ packages/chocolatey/tools/chocolateyUninstall.ps1
@@ -2,33 +2,27 @@ $ErrorActionPreference = 'Stop'
 
 $packageName = 'prompt-pad'
 $softwareName = 'PromptPad*'
-$installerType = 'exe'
-$silentArgs = '/S'
-$validExitCodes = @(0)
+$installerType = 'msi'
+$silentArgs = '/qn /norestart'
+$validExitCodes = @(0, 3010, 1605, 1614, 1641)
 
 $uninstallKey = Get-UninstallRegistryKey -SoftwareName $softwareName
 
 if ($uninstallKey.Count -eq 1) {
   $uninstallKey | ForEach-Object {
-    $uninstallString = $_.UninstallString
+    $productCode = $_.PSChildName
 
     Uninstall-ChocolateyPackage -PackageName $packageName `
                                  -FileType $installerType `
-                                 -SilentArgs $silentArgs `
+                                 -SilentArgs "$silentArgs" `
                                  -ValidExitCodes $validExitCodes `
-                                 -File $uninstallString
+                                 -File $productCode
   }
 } elseif ($uninstallKey.Count -eq 0) {
   Write-Warning "$packageName has already been uninstalled by other means."
 } else {
   Write-Warning "$($uninstallKey.Count) matches found!"
   Write-Warning "To prevent data loss, no programs will be uninstalled."
-}
-
-# Remove Start Menu shortcut
-$shortcutPath = Join-Path $env:ProgramData 'Microsoft\Windows\Start Menu\Programs\PromptPad.lnk'
-if (Test-Path $shortcutPath) {
-  Remove-Item $shortcutPath -Force
 }
 
 Write-Host @"
